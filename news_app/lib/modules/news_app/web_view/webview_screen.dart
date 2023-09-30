d1f355c31d465534c452f18fd7be8ce14061ebbf
elzoghpy <115728094+elzoghpy@users.noreply.github.com>
2023-09-30 20:53:11 +0300

    news

--- news_app/lib/modules/news_app/web_view/webview_screen.dart
+++ news_app/lib/modules/news_app/web_view/webview_screen.dart
@@ -1,15 +1,12 @@
-// ignore_for_file: prefer_const_constructors_in_immutables, use_key_in_widget_constructors, non_constant_identifier_names, unused_import, unnecessary_import
+// ignore_for_file: prefer_const_constructors_in_immutables, use_key_in_widget_constructors, non_constant_identifier_names, unused_import, unnecessary_import, depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:async';
 import 'dart:io';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/widgets.dart';
-import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 class WebViewScreen extends StatelessWidget {
   final String? url;
@@ -24,6 +21,4 @@ class WebViewScreen extends StatelessWidget {
       ),
     );
   }
-
-  WebView({String? initialUrl}) {}
 }
